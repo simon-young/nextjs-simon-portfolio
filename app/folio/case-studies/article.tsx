393f3fd573cb153d13745d46367b32e191a8888b
simon-young <simon@endhunt.com.au>
2023-11-23 11:06:34 +1100

    Add video to content layer

--- app/folio/case-studies/article.tsx
+++ app/folio/case-studies/article.tsx
@@ -15,9 +15,14 @@ export const Article: React.FC<Props> = ({ cases, views }) => {
 			<article className="p-4 md:p-4">
 			{cases.image && (
 				<Suspense fallback={<p>Loading...</p>}>
-					<div className={`relative flex bg-zinc-800 h-[320px] mb-4 rounded-lg overflow-hidden`}>
-						
+					<div className={`relative flex bg-zinc-800 h-[auto] mb-4 rounded-lg overflow-hidden`}>
+						{ cases.video ?
+						<video autoPlay loop>
+							<source src={cases.video} type="video/webm" />
+						</video>
+						:
 						<Image src={`${cases.image}`} alt="top-cases" priority fill className="relative object-cover" />
+						}
 					</div>
                 </Suspense>
 			)};
